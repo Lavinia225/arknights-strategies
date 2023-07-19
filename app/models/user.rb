2be05b5a354916f86c7bf76d9f269a4031a7e581
Luna Seifert <thepuddingdeity@gmail.com>
2023-07-18 20:39:33 -0500

    User validations and password macro added.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-    
+    has_secure_password
+
+    validates :username, uniqueness: true
+    validates :display_name, {uniqueness: true, comparison: {other_than: :username}}
 end
