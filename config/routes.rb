9937b653e1bd6e491413ab7af9234983ede4a937
Luna Seifert <thepuddingdeity@gmail.com>
2023-07-19 21:10:26 -0500

    Created sessions controller, login route, broken logout route, a route to stay loged in, and regular authorization checks.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/hello', to: 'application#hello_world'
+
   
+  get '/me', to: 'users#show'
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   get '*path',
     to: 'fallback#index',
     constraints: ->(req) {!req.xhr? && req.format.html?}
