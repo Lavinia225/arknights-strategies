88b2b340cf0616ee636575c31b3c7cbd2191c2b0
Luna Seifert <thepuddingdeity@gmail.com>
2023-07-19 21:17:50 -0500

    Created Posts migration, model, and controller

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,6 @@ import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
 
   return (
     <BrowserRouter>
