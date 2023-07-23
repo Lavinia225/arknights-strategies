d76c23951397b616a59a7a3eac4c1915e348446c
Luna Seifert <thepuddingdeity@gmail.com>
2023-07-22 21:13:50 -0500

    Updated server's pg (postgresql) gem to use 1.5 to be work with psql 15, and gave Post a show method.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,9 @@ class PostsController < ApplicationController
     def index
         render json: Post.all
     end
+
+    def show
+        post = Post.find_by(id: params[:id])
+        render json: post
+    end
 end
