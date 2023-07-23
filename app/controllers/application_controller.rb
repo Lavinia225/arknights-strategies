d227561a8011f265b5937b4c1f672fe35e3790a3
Luna Seifert <thepuddingdeity@gmail.com>
2023-07-23 18:43:41 -0500

    Unprocessable Entity error rescue method implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
     before_action :authorize
 
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::API
             @current_user = User.find_by(id: session[:user_id])
 
             render json: {errors: ["Not Authorized!"]}, status: :unauthorized unless @current_user
+        end
+
+        def render_unprocessable_entity errors
+            render json: {errors: errors}, status: :unprocessable_entity
         end
 end
