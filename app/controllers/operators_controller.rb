593bf4bc20aa098c19f12da531e10cd4eddddf0e
Luna Seifert <thepuddingdeity@gmail.com>
2023-08-06 15:04:19 -0500

    Created the auth check and first route of Operator Controller{

--- app/controllers/operators_controller.rb
+++ app/controllers/operators_controller.rb
@@ -1,3 +1,14 @@
 class OperatorsController < ApplicationController
+    before_action :admin_check
+    skip_before_action :admin_check, only: :index, :show
+
+    def index
+        render json: Operator.all
+    end
     
+    private
+
+    def admin_check
+         render json: {errors: "You are not authorized to modify this data."}, status: :unauthorized unless @current_user.access_level > 0
+    end
 end
