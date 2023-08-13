0e8bc2ac4e70f415c2f6a5b815967bb338300e2f
Luna Seifert <thepuddingdeity@gmail.com>
2023-08-13 17:18:39 -0500

    Added error handling and refactored code for a cleaner look.

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import {useContext} from 'react'
 import { UserContext } from './context/user'
+import {useHistory} from 'react-router-dom'
 
 function LoginForm(){
     const [formData, setFormData] = useState({
@@ -8,20 +9,33 @@ function LoginForm(){
         password: ""
     })
     const {setUser} = useContext(UserContext)
+    const [errors, setErrors] = useState([])
+    const history = useHistory()
 
-    function handleSubmit(e){
+    async function handleSubmit(e){
         e.preventDefault()
 
-        fetch('/login', {
+        if (formData.username.length < 1 || formData.password.length < 1) return setErrors(["Username or Password is can not be blank."])
+
+        const configObject = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
             },
             body: JSON.stringify(formData)
-        })
-        .then(res => res.json())
-        .then(user =>setUser(user))
+        }
+        
+        const response = await fetch('/login', configObject)
+        const data = await response.json()
+        
+        if (response.ok){
+            setUser(data)
+            history.push('/')
+        }
+        else{
+            setErrors(data.errors)
+        }
     }
 
     function handleChange(e){
@@ -32,13 +46,16 @@ function LoginForm(){
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor='Username'>Username: </label>
-            <input type='text' name='username' value={formData.username} onChange={handleChange}></input>
-            <label htmlFor='Password'>Password: </label>
-            <input type='password' name='password' value={formData.password} onChange={handleChange}></input>
-            <button type='submit'>Submit</button>
-        </form>
+        <>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='Username'>Username: </label>
+                <input type='text' name='username' value={formData.username} onChange={handleChange}></input>
+                <label htmlFor='Password'>Password: </label>
+                <input type='password' name='password' value={formData.password} onChange={handleChange}></input>
+                <button type='submit'>Submit</button>
+            </form>
+            {errors.length > 0 && (errors.map(error => <p key={error} style={{color: 'red'}}>{error}</p>))}
+        </>
     )
 }
 
