54027d128794133b63de0d01032c9ee6e21fff68
Luna Seifert <thepuddingdeity@gmail.com>
2023-08-15 19:28:38 -0500

    Adjusted individual post serialization to include the display name of who created it

--- app/serializers/individual_post_serializer.rb
+++ app/serializers/individual_post_serializer.rb
@@ -1,5 +1,9 @@
 class IndividualPostSerializer < ActiveModel::Serializer
-    attributes :id, :user_id, :body, :title, :created_at, :updated_at
+    attributes :id, :user_id, :creator_display_name, :body, :title, :created_at, :updated_at
     has_many :post_operators
+
+    def creator_display_name
+        "#{object.user.display_name}"
+    end
 end
   
---
+++ client/src/components/Post.js
@@ -0,0 +1,16 @@
+import {useState, useEffect} from 'react'
+
+function Post(){
+    const [post, setPost] = setState()
+    
+    return(
+        <table>
+            <tbody>
+                <tr></tr>
+            </tbody>
+        </table>
+    )
+
+}
+
+export default Post
