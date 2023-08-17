b72383201224358350fa5bde40e900c1f9d48375
Luna Seifert <thepuddingdeity@gmail.com>
2023-08-17 14:08:03 -0500

    Preparing to restructure how operators are handled

--- client/src/components/IndividualOperator.js
+++ client/src/components/IndividualOperator.js
@@ -1,15 +1,16 @@
 import EditOperatorForm from "./EditOperatorForm"
-import { OperatorContext } from "./context/operator"
 import {useParams} from 'react-router-dom'
-import {useContext, useState} from 'react'
+import {useEffect, useState} from 'react'
 
 function IndividualOperator(){
     const params = useParams()
-    const {operators} = useContext(OperatorContext)
     const [editing, setEditing] = useState(false)
     const [operator, setOperator] = useState({name: ""})
 
-
+    useEffect(()=>{
+        //find operator from operators prop
+    })
+    
 }
 
 export default IndividualOperator
