bd4b0c3c806a33828e0211537e056bfab9619c86
Luna Seifert <thepuddingdeity@gmail.com>
2023-08-16 21:14:19 -0500

    Flushed out the new operator form

---
+++ client/src/components/NewOperatorForm.js
@@ -0,0 +1,47 @@
+import {useState, useContext} from 'react'
+import {OperatorContext} from './context/operator'
+
+function NewOperatorForm({handleCreatingStatus}){
+    const [operatorName, setOperatorName] = useState("")
+    const {operators, setOperators, operatorErrors, setOperatorErrors} = useContext(OperatorContext)
+
+    async function handleSubmit(e){
+        e.preventDefault()
+
+        console.log(operatorName)
+        const configObject = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({name: operatorName})
+        }
+
+        const response = await fetch('/operators', configObject)
+        const data = await response.json()
+
+        if (response.ok){
+            setOperators([...operators, data])
+        }
+        else{
+            setOperatorErrors([...operatorErrors, data.errors])
+        }
+
+        handleCreatingStatus()
+    }
+
+    function handleChange(e){
+        setOperatorName(e.target.value)
+    }
+
+    return (
+        <form id='new-operator-form' onSubmit={handleSubmit}>
+            <label htmlFor='name'>Name: </label>
+            <input type='text' onChange={handleChange} value={operatorName}></input>
+            <button type='submit'>Submit</button>
+        </form> 
+    )
+}
+
+export default NewOperatorForm
--- client/src/components/Operators.js
+++ client/src/components/Operators.js
@@ -1,24 +1,40 @@
-import {useContext} from 'react'
+import {useContext, useState} from 'react'
 import { OperatorContext } from "./context/operator"
 import {UserContext} from './context/user'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
+import NewOperatorForm from './NewOperatorForm'
 
 function Operators(){
     const {operators, operatorErrors} = useContext(OperatorContext)
+    const [isCreating, setIsCreating] = useState(false)
     const {user} = useContext(UserContext)
+    const history = useHistory()
 
     function renderOperators(operator){
-        return <li key={`${operator.id} + ${operator.name}`}>{operator.name}</li>
+        return <li key={`${operator.id} + ${operator.name}`} onClick={()=>handleClick(operator.id)}>{operator.name}</li>
+    }
+
+    function handleClick(id){
+        history.push(`/operators/${id}`) //Note that the Operators controller has no edit action right now.
+    }
+
+    function handleCreatingStatus(){
+        setIsCreating(false)
     }
 
     if (user.access_level < 1){
         return <h1 style={{color: 'purple'}}>You are not authorized to be here.</h1>
     }
+
     else{
         return (
-            <ul>
-                {operatorErrors.length > 0 ? operatorErrors.map(error => <li key={error} style={{color: 'red'}}>{error}</li>): null}
-                {operators.length > 0 ? operators.map(renderOperators) : null}
-            </ul>
+            <>
+                {isCreating ? <NewOperatorForm handleCreatingStatus={handleCreatingStatus}/> : <button onClick={()=>setIsCreating(true)}>New Operator</button>}
+                <ul>
+                    {operatorErrors.length > 0 ? operatorErrors.map(error => <li key={error} style={{color: 'red'}}>{error}</li>): null}
+                    {operators.length > 0 ? operators.map(renderOperators) : null}
+                </ul>
+            </>
         )
     }
 }
