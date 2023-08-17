9a219919029d94a8b839c78dd8a712049272fd86
Luna Seifert <thepuddingdeity@gmail.com>
2023-08-17 15:47:40 -0500

    Grabbing operators from the server individually as needed...

--- client/src/components/IndividualOperator.js
+++ client/src/components/IndividualOperator.js
@@ -5,12 +5,24 @@ import {useEffect, useState} from 'react'
 function IndividualOperator(){
     const params = useParams()
     const [editing, setEditing] = useState(false)
+    const [errors, setErrors] = useState([])
     const [operator, setOperator] = useState({name: ""})
 
     useEffect(()=>{
-        //find operator from operators prop
-    })
-    
+        setOperator(getOperator)
+        async function getOperator(){
+            const response = await fetch(`/operators/${params.id}`)
+            const data = await response.json()
+
+            if(response.ok){
+                setOperator(data)
+            }
+            else{
+                setErrors(data)
+            }
+        }
+    }, [])
+    console.log(operator)
 }
 
 export default IndividualOperator
