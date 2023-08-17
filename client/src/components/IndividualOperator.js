1dfb4ab5f67702bf371a3f11c761b6caf5c0bde6
Luna Seifert <thepuddingdeity@gmail.com>
2023-08-17 13:33:25 -0500

    Need a good checkpoint to revert to if this next step does not work.

--- client/src/components/IndividualOperator.js
+++ client/src/components/IndividualOperator.js
@@ -1,7 +1,14 @@
 import EditOperatorForm from "./EditOperatorForm"
 import { OperatorContext } from "./context/operator"
+import {useParams} from 'react-router-dom'
+import {useContext, useState} from 'react'
 
 function IndividualOperator(){
+    const params = useParams()
+    const {operators} = useContext(OperatorContext)
+    const [editing, setEditing] = useState(false)
+    const [operator, setOperator] = useState({name: ""})
+
 
 }
 
