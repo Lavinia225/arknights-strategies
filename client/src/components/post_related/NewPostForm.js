fe6c9ac781cef41a8ee48c1e0c571b1ca1c58c95
Luna Seifert <thepuddingdeity@gmail.com>
2023-08-18 14:49:29 -0500

    Added most of the code unrelated to the actual post request itself

--- client/src/components/post_related/NewPostForm.js
+++ client/src/components/post_related/NewPostForm.js
@@ -1,5 +1,37 @@
-function NewPostForm(){
+import {useState, useContext} from 'react'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
+import { UserContext } from '../context/user'
 
+function NewPostForm(){
+    const history = useHistory()
+    const {user} = useContext(UserContext)
+    const [formData, setFormData] = useState({
+        title: "",
+        body: ""
+    })
+
+    function handleSubmit(){
+
+    }
+
+    function handleChange(e){
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='title'>Title: </label>
+                <input type='text' name='title' value={formData.title} onChange={handleChange} />
+                <label htmlFor='body'>Body: </label>
+                <textarea name='body' rows='20' cols='100' value={formData.body} onChange={handleChange}/>
+                <button type='submit'>Submit</button>
+            </form>
+        </>
+    )
 }
 
 export default NewPostForm
