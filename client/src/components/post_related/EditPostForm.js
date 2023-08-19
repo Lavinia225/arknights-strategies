9c2691a92ab0257aadb4b8e81256dec2ce0b471b
Luna Seifert <thepuddingdeity@gmail.com>
2023-08-18 20:26:22 -0500

    built the async portion of handling the patch request, just need to build out the function in the parent component to pass the data back upstream

--- client/src/components/post_related/EditPostForm.js
+++ client/src/components/post_related/EditPostForm.js
@@ -7,6 +7,28 @@ function EditPostForm({post}){
         body: post.body,
     })
 
+    async function handleSubmit(e){
+        e.preventDefault()
+
+        const configObject = {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(formData)
+        }
+
+        const response = await fetch(`/posts/${post.id}`)
+        const data = await response.json()
+
+        if (response.ok){
+            //call the function provided by Post to update the post in stage and cancel edit state
+        }
+        else{
+            setErrors([data.errors])
+        }
+    }
     function handleChange(e){
         setFormData({...formData, [e.target.name]: e.target.value})
     }
@@ -14,11 +36,12 @@ function EditPostForm({post}){
     return (
         <>
         {errors.length > 0 ? errors.map(error => <p key={error} style={{color: 'red'}}>{error}</p>) : null}
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor='title'>Title: </label>
             <input type='text' name='text' onChange={handleChange} value={formData.title} />
             <label htmlFor='body'>Body: </label>
             <textarea name='body' rows='20' cols='100' onChange={handleChange} value={formData.body}/>
+            <button type='submit'>Submit</button>
         </form>
         </>
     )
