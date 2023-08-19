483898afa55bff85286ca1dd0ff578126fae4e89
Luna Seifert <thepuddingdeity@gmail.com>
2023-08-18 20:40:38 -0500

    Fixed wrong name on a form input

--- client/src/components/post_related/EditPostForm.js
+++ client/src/components/post_related/EditPostForm.js
@@ -29,6 +29,7 @@ function EditPostForm({post, handleUpdatedPost}){
             setErrors([data.errors])
         }
     }
+    
     function handleChange(e){
         setFormData({...formData, [e.target.name]: e.target.value})
     }
@@ -38,7 +39,7 @@ function EditPostForm({post, handleUpdatedPost}){
         {errors.length > 0 ? errors.map(error => <p key={error} style={{color: 'red'}}>{error}</p>) : null}
         <form onSubmit={handleSubmit}>
             <label htmlFor='title'>Title: </label>
-            <input type='text' name='text' onChange={handleChange} value={formData.title} />
+            <input type='text' name='title' onChange={handleChange} value={formData.title} />
             <label htmlFor='body'>Body: </label>
             <textarea name='body' rows='20' cols='100' onChange={handleChange} value={formData.body}/>
             <button type='submit'>Submit</button>
