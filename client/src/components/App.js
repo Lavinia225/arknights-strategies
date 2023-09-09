9291a23e334714acfe56a71cb5e882c57eb9a885
Luna Seifert <thepuddingdeity@gmail.com>
2023-09-09 18:44:51 -0500

    Step one of converting Operator Context to regular state variables centered in App to avoid a pesky bug and reduce how often the backend receives fetch requests

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,3 +1,4 @@
+import {useState, useEffect} from 'react'
 import { Switch, Route } from "react-router-dom";
 import UserBar from './user_related/UserBar'
 import LoginForm from './user_related/LoginForm'
@@ -7,44 +8,59 @@ import Forum from './post_related/Forum'
 import Post from './post_related/Post'
 import NewPostForm from './post_related/NewPostForm'
 import Operators from './operator_related/Operators'
-import {OperatorProvider} from './context/operator'
 import IndividualOperator from "./operator_related/IndividualOperator";
 import Home from './Home'
 
 function App() {
+  const [operators, setOperators] = useState([])
+  const [operatorErrors, setOperatorErrors] = useState([])
+
+  useEffect(()=>{
+    getOperators()
+
+    async function getOperators(){
+      const response = await fetch('/operators')
+      const data = await response.json()
+
+      if (response.ok){
+        setOperators(data)
+      }
+      else{
+        setOperatorErrors(data.errors)
+      }
+    }
+  }, [])
 
   return (
       <div className="App">
         <UserBar />
         <NavBar />
-        <OperatorProvider>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path='/login'>
-              <LoginForm />
-            </Route>
-            <Route path='/signup'>
-              <CreateAccountForm />
-            </Route>
-            <Route exact path='/posts'>
-                <Forum />
-            </Route>
-            <Route path='/posts/new'>
-              <NewPostForm />
-            </Route>
-            <Route path='/posts/:id'>
-              <Post />
-            </Route>
-            <Route path='/operators/:id'>
-              <IndividualOperator />
-            </Route>
-            <Route path='/operators'>
-                <Operators />
-            </Route>
-          </Switch>
-        </OperatorProvider>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path='/login'>
+            <LoginForm />
+          </Route>
+          <Route path='/signup'>
+            <CreateAccountForm />
+          </Route>
+          <Route exact path='/posts'>
+              <Forum />
+          </Route>
+          <Route path='/posts/new'>
+            <NewPostForm />
+          </Route>
+          <Route path='/posts/:id'>
+            <Post />
+          </Route>
+          <Route path='/operators/:id'>
+            <IndividualOperator />
+          </Route>
+          <Route path='/operators'>
+              <Operators />
+          </Route>
+        </Switch>
       </div>
   );
 }
